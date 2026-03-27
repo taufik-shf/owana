91300ad007c747325194986f3694f6139417cad2
taufik-shf <taufik.shf@gmail.com>
2026-03-27 16:25:42 +0700

    ci: configure GitLab Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://taufik-shf1.gitlab.io',
+  base: '/owana-web/',
+});
